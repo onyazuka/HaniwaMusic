6b66f600757f10a9d389f9b5f3d0b6296122d8d4
onyazuka <onizuka2008@yandex.ru>
2025-01-22 11:15:21 +0300

    customized tab bar

--- QTabWidgetCustom.cpp
+++ QTabWidgetCustom.cpp
@@ -2,26 +2,44 @@
 #include <QStyle>
 #include <QStylePainter>
 #include <QStyleOption>
+#include <QApplication>
+#include <QPainterPath>
+
+QTabBarCustom::QTabBarCustom(QWidget *parent)
+    : QTabBar(parent)
+{
+
+}
+
+void QTabBarCustom::paintEvent(QPaintEvent *event) {
+    QStylePainter tabPainter(this);
+    tabPainter.setRenderHint(QPainter::Antialiasing);
+    for (int i = 0; i < count(); ++i) {
+        if (i == currentIndex()) {
+            tabPainter.setPen(QColor(80,80,80));
+            //tabPainter.drawRoundedRect(tabRect(i), 20, 20, Qt::SizeMode::RelativeSize);
+            QPainterPath path;
+            path.addRoundedRect(tabRect(i), 20, 20, Qt::SizeMode::RelativeSize);
+            tabPainter.fillPath(path, QApplication::palette().highlight());
+            tabPainter.drawPath(path);
+            tabPainter.setPen(QApplication::palette().base().color());
+            tabPainter.drawText(tabRect(i), Qt::AlignVCenter | Qt::AlignHCenter, tabText(i));
+        }
+        else {
+            tabPainter.setPen(QColor(80,80,80));
+            //tabPainter.drawRoundedRect(tabRect(i), 20, 20, Qt::SizeMode::RelativeSize);
+            QPainterPath path;
+            path.addRoundedRect(tabRect(i), 20, 20, Qt::SizeMode::RelativeSize);
+            tabPainter.fillPath(path, QApplication::palette().base());
+            tabPainter.drawPath(path);
+            tabPainter.setPen(QApplication::palette().highlight().color());
+            tabPainter.drawText(tabRect(i), Qt::AlignVCenter | Qt::AlignHCenter, tabText(i));
+        }
+    }
+}
 
 QTabWidgetCustom::QTabWidgetCustom(QWidget *parent)
     : QTabWidget(parent)
 {
-    ;
-}
-
-void QTabWidgetCustom::paintEvent(QPaintEvent *event) {
-    /*QStylePainter painter(this);
-    QStyleOptionTab opt;
-
-    for (int i = 0; i < count(); i++)
-    {
-        initStyleOption()
-        opt.palette.setColor(QPalette::Window, Qt::red);
-        opt.palette.setColor(QPalette::Button, Qt::red);
-        painter.drawControl(QStyle::CE_TabBarTabShape, opt);
-        painter.drawControl(QStyle::CE_TabBarTabLabel, opt);
-    }
-    qDebug()<<"window color role color:"<<opt.palette.color(QPalette::Window);
-    qDebug()<<"button color role color:"<<opt.palette.color(QPalette::Button);*/
-    QTabWidget::paintEvent(event);
+    setTabBar(new QTabBarCustom(this));
 }
--- QTabWidgetCustom.hpp
+++ QTabWidgetCustom.hpp
@@ -1,12 +1,18 @@
 #ifndef QTABWIDGETCUSTOM_HPP
 #define QTABWIDGETCUSTOM_HPP
+#include <QTabBar>
 #include <QTabWidget>
+
+class QTabBarCustom : public QTabBar {
+public:
+    QTabBarCustom(QWidget *parent = nullptr);
+protected:
+    void paintEvent(QPaintEvent *event) override;
+};
 
 class QTabWidgetCustom : public QTabWidget {
 public:
     QTabWidgetCustom(QWidget *parent = nullptr);
-protected:
-    void paintEvent(QPaintEvent *event) override;
 };
 
 #endif // QTABWIDGETCUSTOM_HPP
